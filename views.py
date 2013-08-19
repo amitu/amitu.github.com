d1d914eb36b2a2707493d18b0e90579863551b5e
Amit Upadhyay <comments@amitu.com>
2013-08-19 00:33:47 -0700

    post list on index

---
+++ views.py
@@ -0,0 +1,48 @@
+from importd import d
+from path import path
+from djangothis.app import dotslash, read_yaml
+from datetime import datetime
+
+posts = None
+
+def get_posts():
+    global posts
+    if posts is not None: return posts
+    posts = []
+    for post in path(dotslash("_posts")).walkfiles():
+        content = post.open().read()
+        _, header, body = content.split("---", 2)
+        header = read_yaml(header)
+        y, m, d, slug = post.basename().split("-", 3)
+        slug = slug[:-3]
+        posts.append({
+            "date": datetime(int(y), int(m), int(d)),
+            "url": "/%s/%s/%s/" % (y, m, slug),
+            "title": header["title"],
+            "file": (header, body),
+        })
+    posts.sort(lambda x, y: cmp(x["date"], y["date"]), reverse=True)
+    return posts
+
+def context_processor(request):
+    return {
+        "settings": d.settings,
+        "posts": get_posts(),
+    }
+
+def find_post_by_path(pth):
+    prev, curr, nex = None, None, None
+    for post in get_posts():
+        if curr:
+            nex = post
+            break
+        if post["url"] == pth:
+            curr = post
+        else:
+            prev = post
+    return prev, curr, nex
+
+@d("/<int4:year>/<int2:month>/<slug:slug>/")
+def post_page(request, year, month, slug):
+    prev, curr, nex = find_post_by_path(request.path)
+    return [prev, curr, nex]
