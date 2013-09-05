06cb35be0bc1f63ed9065b91457d61f49da22aa8
Amit Upadhyay <comments@amitu.com>
2013-09-05 07:34:40 -0700

    extracted jekyll-theme out

--- _theme/cmds/t2.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-
-class Command(BaseCommand):
-    args = '<poll_id poll_id ...>'
-    help = 'Closes the specified poll for voting'
-
-    def handle(self, *args, **options):
-        for poll_id in args:
-            self.stdout.write('Successfully closed poll "%s"\n' % poll_id)
